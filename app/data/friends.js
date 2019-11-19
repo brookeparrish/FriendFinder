a388a6a46c850b7a65953e526a6da8196423261f
Brooke <parrish.e.brooke@gmail.com>
2019-11-19 16:51:02 -0700

    fixed functionality, still need to clear fields after click

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,7 +18,7 @@ module.exports = [
 
     {
         "name": "Andrew",
-        "photo": "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12231413/Labrador-Retriever-MP.jpg",
+        "photo": "https://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg",
         "scores": [
             2,
             2,
@@ -35,7 +35,7 @@ module.exports = [
 
     {
         "name": "Tyler",
-        "photo": "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12231413/Labrador-Retriever-MP.jpg",
+        "photo": "https://media.mnn.com/assets/images/2019/11/narwhal_puppy_tail_forehead.jpg.653x0_q80_crop-smart.jpg",
         "scores": [
             3,
             3,
@@ -47,6 +47,40 @@ module.exports = [
             3,
             3,
             3
+        ]
+    },
+
+    {
+        "name": "Bella",
+        "photo": "https://cdn-04.independent.ie/regionals/newrossstandard/lifestyle/article38571541.ece/70abc/AUTOCROP/w620/2019-10-08_wex_53943643_I1.JPG",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+
+    {
+        "name": "Oliver",
+        "photo": "https://i.dailymail.co.uk/1s/2019/11/15/10/21047360-7689073-Sl-a-21_1573813313909.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,15 +2,16 @@ const friends = require("../data/friends");
 
 module.exports = function (app) {
     app.get("/api/friends", function (request, response) {
-        console.log(`/api/friends called`);
         response.json(friends);
     });
 
     app.post("/api/friends", function (request, response) {
-        console.log(`POST /api/friends called`);
-        const userResponse = request.body;
+        let userResponse = request.body;
         let userScore = addScore(userResponse.scores);
+        console.log("***USER SCORE***");
+        console.log(userResponse.scores);
         let closeFriend = compareFriends(friends, userScore);
+        
         friends.push(userResponse);
         response.json(closeFriend);        
     });
@@ -23,10 +24,15 @@ function compareFriends(arr, userResponseScore) {
     for(let friend of arr) {
         let totalFriendScore = addScore(friend.scores);
 
-
         let difference = Math.abs(userResponseScore - totalFriendScore);
+        console.log("**** Difference ***");
+        console.log(difference);
+        console.log("**** Close Difference ***");
+        console.log(closeDifference);
 
-        if (closeDifference === null || difference < closeDifference) {
+        if (closeDifference === null || difference <= closeDifference) {
+            console.log(friend.name);
+            console.log(difference);
             closeDifference = difference;
             closestFriend = friend; 
         }
@@ -37,6 +43,10 @@ function compareFriends(arr, userResponseScore) {
 function addScore(arr) {
     let total = 0;
     for(let i = 0; i < arr.length; i++) {
+        if(typeof arr[i] === "string") {
+            arr[i] = +arr[i]
+        }
+
         total += arr[i];
     }
     return total;
